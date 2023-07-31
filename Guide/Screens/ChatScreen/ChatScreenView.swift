9892d9aa53e65b8c05f03a4df5649fecdf6e0c2c
Shivam Rawat <shivamrawat6670@gmail.com>
2023-08-01 00:26:14 +0530

    Added colors to the swift UI.

--- Guide/Screens/ChatScreen/ChatScreenView.swift
+++ Guide/Screens/ChatScreen/ChatScreenView.swift
@@ -26,14 +26,10 @@ struct Header: View {
 
 private struct ChatBubbleView : View {
     var message: ChatMessage ;
-    init(message: ChatMessage) {
-        self.message = message;
-    }
-    
     @State var selected : Bool = false;
     
     var backgroundColor : Color {
-        selected ? Color.blue : Color.gray;
+        selected ? Color.blue : Color("chat-default");
     }
     
     var profileImage : some View {
@@ -45,11 +41,10 @@ private struct ChatBubbleView : View {
     
     var textBubble : some View  {
         Text(message.message)
-            .foregroundStyle(.white)
+            .foregroundStyle(selected ? .white : .black)
             .padding(.vertical,15)
             .padding(.horizontal,24)
             .frame(minWidth: 0,maxWidth: 230,alignment:.leading)
-        
     }
     
     var sendMessage : some View {
@@ -67,6 +62,20 @@ private struct ChatBubbleView : View {
         }
     }
     
+    var messageOptions : some View {
+        HStack {
+            Image(systemName: "face.dashed.fill")
+                .foregroundColor(.orange)
+            Image(systemName: "paperplane.fill")
+                .foregroundColor(.blue)
+            Image(systemName: "ellipsis")
+                .foregroundColor(.teal)
+        }
+        .padding()
+        .background(Color("chat-default"))
+        .clipShape(RoundedRectangle(cornerRadius: 24))
+    }
+    
     var recievedMessage : some View {
         HStack (alignment: .top){
             VStack{
@@ -74,22 +83,13 @@ private struct ChatBubbleView : View {
                 Text(message.time)
                     .font(.system(size: 12))
             }
-            VStack{
+            VStack(alignment:.trailing){
                 textBubble
                     .background(backgroundColor)
                     .clipShape(MessageBubble())
+                    .shadow(radius: selected ?  6: 0)
                 if selected {
-                    HStack {
-                        Image(systemName: "face.dashed.fill")
-                            .foregroundColor(.orange)
-                        Image(systemName: "paperplane.fill")
-                            .foregroundColor(.blue)
-                        Image(systemName: "ellipsis")
-                            .foregroundColor(.teal)
-                    }
-                    .padding()
-                    .background(.gray)
-                    .clipShape(RoundedRectangle(cornerRadius: 24))
+                    messageOptions
                 }
             }
         }
@@ -109,31 +109,67 @@ private struct ChatBubbleView : View {
 
 private struct Chat: View {
     @StateObject var viewModel = ChatScreenViewModel();
+    
+    var typing : some View {
+        HStack (alignment: .top){
+            VStack{
+                AsyncImage(url: URL(string:"https://i.pravatar.cc/150?img=3"))
+                    .scaledToFit()
+                    .frame(width: 50,height: 50)
+                    .clipShape(Circle())
+                
+            }
+            HStack {
+                Circle()
+                    .foregroundStyle(.blue)
+                    .frame(width: 10)
+                    .opacity(1)
+                Circle()
+                    .foregroundStyle(.blue)
+                    .frame(width: 10)
+                    .opacity(0.7)
+                Circle()
+                    .foregroundStyle(.blue)
+                    .frame(width: 10)
+                    .opacity(0.3)
+            }
+            .padding()
+            .background(Color("chat-default"))
+            .clipShape(MessageBubble())
+        }
+    }
+    
     var body : some View {
         ScrollView {
             LazyVStack(alignment: .leading){
                 ForEach(viewModel.messages){message in
                     ChatBubbleView(message: message)
                 }
+                typing
             }
         }
     }
 }
 
-struct Input: View {
+private struct Input: View {
     @State var input : String = "";
     var body : some View {
         HStack{
             Image(systemName: "plus")
+                .font(.system(size: 14))
                 .foregroundColor(.white)
-                .padding()
-                .background(.blue)
+                .padding(6)
+                .background(Gradient(colors: [Color(red: 0.0, green: 1.0, blue: 1.0),Color(red: 0.0, green: 0.2, blue: 0.7)]))
                 .clipShape(Circle())
             TextField("Type your message...", text: $input )
             Image(systemName: "face.smiling")
-            
         }
-        .background(.gray)
+        // ASK: Why do we need to do multiple padding
+        .padding(.vertical, 10)
+        .padding(.horizontal,8)
+        .background(Color("chat-default"))
+        .clipShape(RoundedRectangle(cornerRadius: 25))
+        .padding()
     }
 }
 
@@ -141,19 +177,10 @@ struct ChatScreenView: View {
     var body: some View {
         VStack{
             Header()
-            Spacer()
             Chat()
-                .padding(.vertical,20)
+                .padding(.top,20)
                 .padding(.horizontal,10)
             Input()
-            
         }
-    }
-}
-
-struct ChatScreenView_Preview : PreviewProvider {
-    
-    static var previews : some View {
-        ChatScreenView()
     }
 }
